9d430fac68be9b1e7d900258872df074cfeb9506
SomnathB053 <bose.somnath2409@gmail.com>
2026-03-05 01:32:01 +0530

    rearrange function

--- src/eval.py
+++ src/eval.py
@@ -7,25 +7,7 @@ from torch.utils.data import DataLoader
 
 from dataset import SegmentationDataset
 from model import SegmentationModel
-
-
-def color_map(N=256, normalized=False):
-    def bitget(byteval, idx):
-        return ((byteval & (1 << idx)) != 0)
-    dtype = 'float32' if normalized else 'uint8'
-    cmap = np.zeros((N, 3), dtype=dtype)
-    for i in range(N):
-        r = g = b = 0
-        c = i
-        for j in range(8):
-            r = r | (bitget(c, 0) << 7-j)
-            g = g | (bitget(c, 1) << 7-j)
-            b = b | (bitget(c, 2) << 7-j)
-            c = c >> 3
-        cmap[i] = np.array([r, g, b])
-    cmap = cmap/255 if normalized else cmap
-    return cmap
-
+from utils import color_map
 
 
 def run_inference(model, test_loader, folder_name, model_path='model_checkpoints/model.pth'):
